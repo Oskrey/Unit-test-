6044e0bc60622aa83420ba2b86c1bf1bae17dec3
Никита Педич <pedichn@gmail.com>
2022-04-06 17:23:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Unit_test_ИС
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            connection = new SqlConnection(); //Создание объекта подключения
+            connection.ConnectionString = @"Data Source=DESKTOP-467M70L\SQLEXPRESS;Initial Catalog=UnitTest;Integrated Security=True;Connect Timeout=5;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";  //Строка подключения
+            try
+            {
+                connection.Open();      //Опасная команда
+                MessageBox.Show("Связь с сервером установлена");
+                Application.Run(new Form1());
+
+            }
+            catch (SqlException ex)     //Обработка сбоя при подключении
+            {
+                switch (ex.Number)      //Номер ошибки
+                {
+                    case 17: MessageBox.Show("Неверное имя сервера"); break;
+                    case 4060: MessageBox.Show("Неверное имя БД"); break;
+                    case 18456: MessageBox.Show("Неверное имя пользователя или пароль"); break;
+                }
+                MessageBox.Show(ex.Message + Environment.NewLine + "Уровень ошибки " + ex.Class); return;
+            }
+            catch (Exception ex)            //Общий сбой при подключении
+            {
+                MessageBox.Show("Ошибка подключения " + ex.Message); return;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open) connection.Close();
+            }
+        }
+        public static SqlConnection connection;
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Unit test ИС")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unit test ИС")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5ce30e51-74b9-4505-a3c0-072d253d4339")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
